756fef793628788c21b7d4dbe808d926f95c978d
Vera Preymachuk <veragen@mail.ru>
2022-02-13 21:27:04 +0700

    added testng

---
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -0,0 +1,35 @@
+package ru.netology.service;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import static org.testng.Assert.*;
+
+
+public class CashbackHackServiceTest {
+
+    @Test
+    public void shouldCalculateIfAmountTrue() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 5300;
+        int actual = cashbackHackService.remain(amount);
+        int expected = 700;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldCalculateIfAmountFalse() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 1000;
+        int actual = cashbackHackService.remain(amount);
+        int expected = 1000;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void shouldCalculateIfAmountZero() {
+        CashbackHackService cashbackHackService = new CashbackHackService();
+        int amount = 0;
+        int actual = cashbackHackService.remain(amount);
+        int expected = 1000;
+        assertEquals(expected, actual);
+    }
+}
