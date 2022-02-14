4b1a82a46e3b4e8577a0f92493aebdb87c2fe41c
Vera Preymachuk <veragen@mail.ru>
2022-02-14 23:29:25 +0700

    failed test

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -29,7 +29,7 @@ public class CashbackHackServiceTest {
         CashbackHackService cashbackHackService = new CashbackHackService();
         int amount = 0;
         int actual = cashbackHackService.remain(amount);
-        int expected = 1000;
+        int expected = 0;
         assertEquals(expected, actual);
     }
 }
